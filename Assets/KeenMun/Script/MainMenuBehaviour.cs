4c4366962ada1c3b39bf0db0f6011ce7ff40d351
JokeChu <robect2@gmail.com>
2023-07-31 22:55:58 +0800

    Main Menu Update
    Overall function done, except for:
    1. What to put into options

---
+++ Assets/KeenMun/Script/MainMenuBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName) ;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
