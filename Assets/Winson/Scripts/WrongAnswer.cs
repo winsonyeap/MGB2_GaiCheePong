6311ebde0a04464c1b2118391550410310bd3305
JokeChu <robect2@gmail.com>
2023-08-18 23:18:45 +0800

    hehe
    hehe

--- Assets/Winson/Scripts/WrongAnswer.cs
+++ Assets/Winson/Scripts/WrongAnswer.cs
@@ -26,19 +26,10 @@ public class WrongAnswer : MonoBehaviour
         {
             gameOver = true;
             otherOption.GetComponent<WrongAnswer>().gameOver = true;
-            //ParticleSystem Explosion = GameObject.Find("Rocket").GetComponent<ParticleSystem>();
-            //Explosion.Play();
             var spawn= Instantiate(Explosion, other.transform.position, other.transform.rotation);
             spawn.GetComponent<ParticleSystem>().Play();
             Destroy(other.gameObject);
             FindObjectOfType<PauseMenuBehaviour>().GameOver(); //JokeChu function
-
-            //FindObjectOfType<PauseMenuBehaviour>().GameOver();
-            //gameOver = true;
-            //otherOption.GetComponent<WrongAnswer>().gameOver = true;
-            //Time.timeScale = 0;
-            //FindObjectOfType<PauseMenuBehaviour>().GameOver(); //JokeChu function
-            ////level failed when collide with wrong answer
         }
 
         if (gcScript.limited && gcScript.numOfQuestions == gcScript.numOfQuestionsAsked && other.CompareTag("Player") && !IsWrong && !gameOver)
